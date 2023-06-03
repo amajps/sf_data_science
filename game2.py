d6d47ff21f9727d683fff3b244e4a6f2f5323310
amajps <amajps1@gmail.com>
2023-06-03 21:38:16 +0500

    Initial commit :)

---
+++ game2.py
@@ -0,0 +1,30 @@
+""" игра компьютер угадывает число рандомно"""
+import numpy as np
+
+def random_predict(number=1) -> int:
+    """_summary_
+
+    Args:
+        num (int, optional): заданное число. Defaults to 1.
+
+    Returns:
+        int: число попыток
+    """
+    count=0
+    while True:
+        numr=np.random.randint(0, 100)
+        count+=1
+        if numr == number:
+            return count
+
+
+def score_game(random_predict) ->int:
+    count_ls=[]
+    np.random.seed(1)
+    random_array=  np.random.randint(0,100,size=(1800))
+    for nummer in random_array:
+        count_ls.append(random_predict(nummer))
+    score = int(np.mean(count_ls))
+    return score   
+if __main__ == '__name__':
+    print(score_game(random_predict))   
